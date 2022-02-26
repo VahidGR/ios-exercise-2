a3ea5d019c4c63a403fddc5a197e16abadafc3ea
Vahid Ghanbarpour <cahidgr.vahid@gmail.com>
2022-02-26 21:08:32 +0330

    create timestamp generator class to make timestamps

---
+++ Exercise/TimestampGenerator.swift
@@ -0,0 +1,27 @@
+//
+//  TimestampGenerator.swift
+//  Exercise
+//
+//  Created by Vahid Ghanbarpour on 2/26/22.
+//  Copyright © 2022 Matthias Nagel. All rights reserved.
+//
+
+import Firebase
+
+final class TomestampGenerator {
+    static func createTimestamp(year: Int, month: Int = 1, day: Int = 1) -> Timestamp {
+        var dateComponents = DateComponents()
+        dateComponents.year = year
+        dateComponents.month = month
+        dateComponents.day = day
+        dateComponents.timeZone = TimeZone(abbreviation: "UTC")
+        dateComponents.hour = 0
+        dateComponents.minute = 0
+
+        // Create date from components
+        let userCalendar = Calendar(identifier: .gregorian)
+        let date = userCalendar.date(from: dateComponents)!
+        
+        return Timestamp(date: date)
+    }
+}
