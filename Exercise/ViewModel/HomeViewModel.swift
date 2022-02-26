813e8979481249b2578cd5a3b6a433def31e0d32
Vahid Ghanbarpour <cahidgr.vahid@gmail.com>
2022-02-27 02:24:04 +0330

    update how projects work

--- Exercise/ViewModel/HomeViewModel.swift
+++ Exercise/ViewModel/HomeViewModel.swift
@@ -6,51 +6,21 @@
 //  Copyright © 2022 Matthias Nagel. All rights reserved.
 //
 
+import Foundation
+import Combine
 import Firebase
 
 class HomeViewModel {
     // controller
     var delegate: Controller?
     
-    private let firestore = Firestore.firestore()
-    var projects = [Project]()
+    private let db = Firestore.firestore()
+    var projects = Projects()
     // error message
-    private var message: String? {
+    private var error: Error? {
         didSet {
-            guard let message = message else { return }
+            guard let message = error?.localizedDescription else { return }
             delegate?.showError(message: message)
-        }
-    }
-    
-    // MARK: - Load projects
-    func loadProjects() {
-        
-        self.projects.removeAll()
-        let year2020 = TimestampGenerator.createTimestamp(year: 2020)
-        let year2019 = TimestampGenerator.createTimestamp(year: 2019)
-        firestore.collection("projects")
-            .whereField("timestamp", isLessThan: year2020)
-            .whereField("timestamp", isGreaterThan: year2019)
-            .getDocuments { [weak self] snapshot, error in
-            if error == nil {
-                if let snapshot = snapshot {
-                    for document in (snapshot.documents) {
-                        if let model = self?.decodeToModel(data: document.data()) {
-                            self?.projects.append(model)
-                        }
-                    }
-                    
-                    DispatchQueue.main.async {
-                        self?.delegate?.updateUI()
-                    }
-                    self?.startListening()
-                } else {
-                    self?.message = "Invalid snapshot"
-                }
-            }
-            else {
-                self?.message = error?.localizedDescription
-            }
         }
     }
     
@@ -63,33 +33,17 @@ class HomeViewModel {
             "timestamp": Timestamp(date: Date())
         ]
         
-        firestore.collection("projects").addDocument(data: data)
+        db.collection("projects").addDocument(data: data)
     }
 
     // MARK: - Listen to changes
-    private func startListening() {
-        firestore.collection("projects").addSnapshotListener { [weak self] snapshot, error in
+    func startListening() {
+        db.collection("projects").addSnapshotListener { [weak self] snapshot, error in
             if error == nil {
                 snapshot?.documentChanges.forEach { diff in
                     let document = diff.document.data()
                     if (diff.type == .added) {
-                        if let _ = self?.projects.firstIndex(where: { $0.id == document["id"] as? String }) { } else {
-                            if let model = self?.decodeToModel(data: document) {
-                                self?.projects.append(model)
-                            }
-                        }
-                    }
-                    if (diff.type == .modified) {
-                        if let index = self?.projects.firstIndex(where: { $0.id == document["id"] as? String }) {
-                            if let model = self?.decodeToModel(data: document) {
-                                self?.projects[index] = model
-                            }
-                        }
-                    }
-                    if (diff.type == .removed) {
-                        if let index = self?.projects.firstIndex(where: { $0.id == document["id"] as? String }) {
-                            self?.projects.remove(at: index)
-                        }
+                        self?.projects.update(insert: document)
                     }
                 }
                 DispatchQueue.main.async {
@@ -97,22 +51,37 @@ class HomeViewModel {
                 }
             }
             else {
-                self?.message = error?.localizedDescription
+                self?.error = error
             }
         }
     }
     
-    // MARK: - Decode recieved value to a Codable object
-    private func decodeToModel(data: [String: Any]) -> Project {
-        let id = data["id"] as? String
-        let title = data["title"] as? String
-        let timestamp = data["timestamp"] as? Timestamp
-        let date = timestamp!.dateValue()
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd"
-        let formattedTime = formatter.string(from: date)
+    class Projects {
+        var data: [Project]
+        var generator = TimestampGenerator()
         
-        return Project(id: id, title: title, timestamp: formattedTime)
+        init(data: [Project] = [])
+        {
+            self.data = data
+        }
+        
+        func update(insert document: Project) {
+            data.append(document)
+        }
+        
+        func update(insert data: [String: Any]?) {
+            if let document = parse(remote: data) {
+                update(insert: document)
+            }
+        }
+        
+        private func parse(remote: [String: Any]?) -> Project? {
+            let id = remote?["id"] as? String
+            let title = remote?["title"] as? String
+            let timestamp = remote?["timestamp"] as? Timestamp
+            
+            return Project(id: id, title: title, timestamp: timestamp?.formattedTime)
+        }
     }
 
 }
