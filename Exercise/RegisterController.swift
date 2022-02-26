4e931bd17319a35a7d6fe5ae7c0918f55ce368f6
Vahid Ghanbarpour <cahidgr.vahid@gmail.com>
2022-02-26 19:21:58 +0330

    use keyboard configurable protocol on register view controller

--- Exercise/RegisterController.swift
+++ Exercise/RegisterController.swift
@@ -13,8 +13,9 @@ final class RegisterController: UIViewController {
 
 	@IBOutlet private weak var emailTextField: UITextField!
 	@IBOutlet private weak var passwordTextField: UITextField!
-
-	@IBAction private func doClose(_ sender: UIBarButtonItem) {
+    @IBOutlet weak var topConstraint: NSLayoutConstraint!
+    
+    @IBAction private func doClose(_ sender: UIBarButtonItem) {
 		self.dismiss(animated: true)
 	}
 
@@ -24,5 +25,38 @@ final class RegisterController: UIViewController {
 
 		self.performSegue(withIdentifier: "showHome", sender: nil)
 	}
+    
+    var constraintToChange: NSLayoutConstraint? {
+        get {
+            return topConstraint
+        }
+    }
+    
+    var original_constant: CGFloat?
+    
+    override func loadView() {
+        super.loadView()
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureKeyboard()
+        tap = UITapGestureRecognizer(target: self, action: #selector(dismiss(_:)))
+    }
 }
 
+extension RegisterController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return false
+    }
+}
+
+extension RegisterController: KeyboardConfigurable, UIGestureRecognizerDelegate {
+    @objc
+    private func dismiss(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+}
