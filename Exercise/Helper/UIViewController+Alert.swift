da6da7fadbc9a1824d12dfde2ed490040e8fe47b
Vahid Ghanbarpour <cahidgr.vahid@gmail.com>
2022-02-27 02:13:09 +0330

    add action option to alert helper

--- Exercise/Helper/UIViewController+Alert.swift
+++ Exercise/Helper/UIViewController+Alert.swift
@@ -9,9 +9,11 @@
 import UIKit
 
 extension UIViewController {
-    func alert(title: String, message: String, action: String) {
+    func alert(title: String, message: String, action: String, completion: (() -> ())? = nil) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: action, style: .cancel) { _ in })
+        alert.addAction(UIAlertAction(title: action, style: .cancel) { _ in
+            completion?()
+        })
         present(alert, animated: true){}
     }
 }
